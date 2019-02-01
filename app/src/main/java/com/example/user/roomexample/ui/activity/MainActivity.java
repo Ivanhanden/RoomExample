39d7b805dfbf058e80d1ec42c9b6de0edb31d82e
arzumanyasha <arzumanianartur0@gmail.com>
2019-02-01 23:58:24 +0300

    Change adapter

--- app/src/main/java/com/example/user/roomexample/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/user/roomexample/ui/activity/MainActivity.java
@@ -2,16 +2,13 @@ package com.example.user.roomexample.ui.activity;
 
 
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.user.roomexample.R;
 import com.example.user.roomexample.data.database.AppDatabase;
 import com.example.user.roomexample.data.entity.Note;
-import com.example.user.roomexample.ui.adapter.NotesAdapter;
+import com.example.user.roomexample.ui.adapter.NoteAdapter;
 import com.example.user.roomexample.ui.viewmodel.NoteViewModel;
 
 import java.util.List;
@@ -22,66 +19,29 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends BaseActivity implements NotesAdapter.OnItemClickListener {
+public class MainActivity extends BaseActivity {
 
     private NoteViewModel noteViewModel;
-    private EditText titleEditText;
-    private EditText bodyEditText;
-
-    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
 
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
-
-        titleEditText = findViewById(R.id.titleEditText);
-        bodyEditText = findViewById(R.id.bodyEditText);
-        Button addButton = findViewById(R.id.button);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!TextUtils.isEmpty(titleEditText.getText().toString())
-                        && !TextUtils.isEmpty(bodyEditText.getText().toString())) {
-                    AppDatabase.getAppDatabase(MainActivity.this).noteDao().insert(new Note(titleEditText.getText().toString(), bodyEditText.getText().toString()));
-                    List<Note> noteList = getNoteList();
-                    notesAdapter.updateList(noteList);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Empty note", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-        restoreData();
-    }
-
-    private void restoreData() {
-        updateUI(getNoteList());
-    }
-
-    private void updateUI(List<Note> noteList) {
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setVisibility(View.VISIBLE);
-        recyclerView.setHasFixedSize(true);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        notesAdapter = new NotesAdapter(noteList, MainActivity.this);
-        recyclerView.setAdapter(notesAdapter);
-    }
-
-    private List<Note> getNoteList() {
-        return AppDatabase.getAppDatabase(MainActivity.this).noteDao().getAll();
     }
 
     @Override
